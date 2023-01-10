e55365c21c2b36f2598b0332e2c8a6ecfc725ed9
Sergey Kykhov <ghrle@mail.ru>
2023-01-10 18:30:32 +0300

    Initial Commit

---
+++ Hero table Dota3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hero table Dota3
+//
+//  Created by Sergey Kykhov on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
